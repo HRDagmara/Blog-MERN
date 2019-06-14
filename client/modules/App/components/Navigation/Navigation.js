7b0d5008115174656979e1073505e8e685d58e7c
Dagmara Bełczowska <dagmara.belczowska@wp.pl>
2019-06-14 18:38:46 +0200

    Add navigation

--- client/modules/App/components/Navigation/Navigation.js
+++ client/modules/App/components/Navigation/Navigation.js
@@ -1,6 +1,13 @@
 import React, { PropTypes } from 'react';
 
-const Hello= Navigation = (props, context) => {
+const Navigation = () => {
+    return (
+        <div className={navigation}>
+            <h2>Home</h2>
+            <h2>Posts</h2>
+            <h2>About</h2>
+        </div>
+    );
 }
 
 Navigation.propTypes = {
